fb4452614560821903071de85e880453e2598c3b
Daniel Roth <daniel@encodo.ch>
2015-11-18 13:51:14 +0100

    Updated packages

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,10 +4,11 @@ import * as React from 'react';
 import './seasons.less';
 import {HistoryLocation } from 'react-router';
 import * as Router2 from 'react-router';
+import * as ReactDom from 'react-dom';
 //import default as Router2 from 'react-router';
 import routes from 'shared/routes';
 
 var uncastedRouter:any = Router2;
 var Router:React.ComponentClass<any> = uncastedRouter.Router;
 
-React.render(<Router history={Router2.HistoryLocation}>{routes}</Router>, document.getElementById('content'));
+ReactDom.render(<Router history={Router2.HistoryLocation}>{routes}</Router>, document.getElementById('content'));
