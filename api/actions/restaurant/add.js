60391d39f1dfb4364a3b4639c3aa7a97fbd8da6c
Daniel Roth <daniel@encodo.ch>
2015-11-25 15:18:34 +0100

    Minor validation

--- api/actions/restaurant/add.js
+++ api/actions/restaurant/add.js
@@ -4,11 +4,14 @@ export default function add(req) {
   return new Promise((resolve, reject) => {
     // write to database
     setTimeout(() => {
-      if (Math.random() < 0.2) {
+      const restaurant = req.body;
+      if (restaurant.id) {
+        reject('Restaurant has an id assigned and cannot be created again.');
+      }
+      else if (Math.random() < 0.2) {
         reject('Oh no! Restaurant add fails 20% of the time. Try again.');
       } else {
         const restaurants = getRestaurants(req);
-        const restaurant = req.body;
         restaurant.id = restaurants.slice(0).sort((left, right) => right.id - left.id)[0] + 1;
         restaurants.push(restaurant);
 
